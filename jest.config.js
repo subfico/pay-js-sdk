728bb5200bf680168a5668ad8286f9415fd3e602
Nikita Tomkevich <nikita@anedot.com>
2024-09-12 10:46:00 -0400

    main: clean up jest.config

--- jest.config.js
+++ jest.config.js
@@ -7,29 +7,23 @@ module.exports = {
 
   // Collect code coverage from these files
   collectCoverageFrom: [
-    '!src/**/*.js',    // Collect coverage from all JS files in the 'src' folder
-    '!src/**/*.test.js'  // Ignore test files when collecting coverage
+    '!src/**/*.js',
+    '!src/**/*.test.js'
   ],
 
   // Automatically clear mock calls, instances, contexts and results before every test
   clearMocks: true,
 
   // Module file extensions for importing
-  moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
-
-  // Babel transformation for modern JavaScript features
-  transform: {
-    '^.+\\.(js|jsx)$': 'babel-jest',  // Use babel-jest to transpile JavaScript files
-  },
+  moduleFileExtensions: ['js', 'json', 'node'],
 
   // Configure Jest to look for test files
   testMatch: [
-    '**/__tests__/**/*.js',       // Any files inside __tests__ folders
-    '**/?(*.)+(spec|test).js',    // Any files with .test.js or .spec.js
+    // Any files inside __tests__ folders
+    '**/__tests__/**/*.js',
+    // Any files with .test.js or .spec.js
+    '**/?(*.)+(spec|test).js',
   ],
-
-  // Set up global test setup, if needed
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   
   // Verbose output of test results
   verbose: true,
